f178e6c89dfc609d6f254cd691e451c158832675
Earl Platt III <eaplatt3@gmail.com>
2018-04-26 13:19:48 -0400

    Outputting the pixel color for x and y values

--- MazeProgram/src/MazeTest.java
+++ MazeProgram/src/MazeTest.java
@@ -4,8 +4,10 @@ import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MazeTest extends Application {
@@ -14,8 +16,9 @@ public class MazeTest extends Application {
 	public final static int FADE = 8;
 	private StackPane spn2;
 	private StackPane spn1;
-	private NumberAxis xAxis;
-	
+	private PixelReader pr;
+	private Color color;
+
 
 	@Override
 	public void start(Stage stage) throws Exception {
@@ -25,22 +28,29 @@ public class MazeTest extends Application {
 		ImageView imgViewM = new ImageView(Ming);
 		Image Ding = new Image("droid.jpg");
 		ImageView imgViewD = new ImageView(Ding);
-		
+
 
 		spn1 = new StackPane(imgViewM);
 		spn2 = new StackPane(imgViewD);
-		
-		
-		
+
 
 		//Starting Location for Droid
 		spn2.setLayoutX(15);
 		spn2.setLayoutY(258);
-		
+
 		Group g = new Group(spn1, spn2);
 
 		Scene sn = new Scene(g, 607, 421);
-		sn.setOnKeyPressed(this::processKeyPress);
+
+
+		this.readPixelColor(Ming);
+
+		if(color != Color.WHITE) {
+
+			sn.setOnKeyPressed(this::processKeyPress);
+
+		}
+
 		stage.setScene(sn);
 		stage.setResizable(false);
 		stage.setTitle("Maze Game");
@@ -49,9 +59,35 @@ public class MazeTest extends Application {
 
 	}
 
+	//Reads in the Color of each pixel per X and Y value then Prints them 
+	private void readPixelColor(Image Ming) {
+
+		int width = (int)Ming.getWidth();
+		int height = (int)Ming.getHeight();
+		int y;
+		int x = 0;
+
+		pr = Ming.getPixelReader();
+
+		for(y = 0; y < height; y++) {
+
+			for(x = 0; x < width; x++) {
+				color = pr.getColor(x, y);
+
+				System.out.println("Color y: " + y + " " + color + " Color x: " + x + " " + color);
+
+			}
+
+		}
+
+
+	}
+
+
+
 	//Key Press Method
 	public void processKeyPress(KeyEvent event) {
-
+				
 		switch (event.getCode()) {
 
 		case UP:
@@ -60,7 +96,7 @@ public class MazeTest extends Application {
 		case DOWN:
 			spn2.setLayoutY(spn2.getLayoutY() + MOVE);
 			break;
-	    case RIGHT:
+		case RIGHT:
 			spn2.setLayoutX(spn2.getLayoutX() + MOVE);
 			break;
 		case LEFT:
@@ -72,6 +108,7 @@ public class MazeTest extends Application {
 		}		
 
 	}
+
 
 	public static void main(String[] args) {
 		launch(args);
